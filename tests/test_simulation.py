2b0f7e889cfcf02a9caeaa8f44a9e802cd05c003
Eden Grown-Haeberli <edengh@gmail.com>
2020-07-15 07:44:30 -0700

    test(simulation): adds preliminary tests for simulation

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,4 +1,15 @@
 __author__ = "Eden Grown-Haeberli"
 
+from tidepool_data_science_simulator.makedata.make_simulation import get_canonical_simulation
+from tidepool_data_science_simulator.makedata.make_patient import DATETIME_DEFAULT
+
 def test_simulation():
-    pass
+
+    t0, sim = get_canonical_simulation()
+
+    # Initialization checks
+    assert sim.time == DATETIME_DEFAULT
+    assert sim.duration_hrs == 8
+    #assert sim.simulation_results[t0]
+
+    # Run checks
