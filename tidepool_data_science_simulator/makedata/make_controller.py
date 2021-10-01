d7a23ca3b6276bf6b6ce654d5f0b4184e30382cd
ihadanidea <shawnmfoster@gmail.com>
2021-10-01 13:02:27 -0700

    additional carb absorption times

--- tidepool_data_science_simulator/makedata/make_controller.py
+++ tidepool_data_science_simulator/makedata/make_controller.py
@@ -20,7 +20,7 @@ def get_canonical_controller_config(t0=DATETIME_DEFAULT):
         "rate_rounder": 0.05,
         "insulin_delay": 10,
         "carb_delay": 10,
-        "default_absorption_times": [120.0, 180.0, 240.0],
+        "default_absorption_times": [30.0, 120.0, 180.0, 240.0, 300.0],
         "max_basal_rate": 35,
         "max_bolus": 30,
         "retrospective_correction_enabled": True
