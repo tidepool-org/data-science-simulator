fd428162bce00e6d1351ed7adfba17cf1154ca81
ihadanidea <shawnmfoster@gmail.com>
2024-09-11 10:21:21 -0700

    additional subdirectory for removing Child Insulin Model re-run
    Signed-off-by: ihadanidea <shawnmfoster@gmail.com>

--- tidepool_data_science_simulator/projects/risk/loop_risk_v2_0.py
+++ tidepool_data_science_simulator/projects/risk/loop_risk_v2_0.py
@@ -48,7 +48,7 @@ def build_risk_sim_generator(scenario_json_filepath, override_config_save_dir=No
     for risk_dir_name in risk_dirs:
         print(f"Processing risk directory: {risk_dir_name}")
         # for use in filtering to just one risk. If wanting to run all of them, comment out lines 35-37
-        if ("TLR-1034") not in risk_dir_name:
+        if ("TLR-548") not in risk_dir_name:
             print(f"Skipping {risk_dir_name} as it doesn't contain 'TLR-000'")
             continue
         print(f"Processing: {risk_dir_name}")
