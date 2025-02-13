aa8b89e0289e813533c934316ebbaade60a3a856
markjudeconnolly <159162013+markjudeconnolly@users.noreply.github.com>
2025-02-13 13:52:11 -0500

    initial check in

--- tidepool_data_science_simulator/projects/autobolus_application_factor/analyze_autobolus_sweep.py
+++ tidepool_data_science_simulator/projects/autobolus_application_factor/analyze_autobolus_sweep.py
@@ -6,61 +6,112 @@ from tidepool_data_science_simulator.utils import DATA_DIR
 import glob
 import pandas as pd
 from tidepool_data_science_simulator.evaluation.inspect_results import load_results, load_result, collect_sims_and_results
-from tidepool_data_science_metrics.glucose.glucose import percent_values_ge_70_le_180, percent_values_lt_70, percent_values_gt_180
+from tidepool_data_science_metrics.glucose.glucose import percent_values_ge_70_le_180, percent_values_ge_70_le_140, percent_values_lt_70, percent_values_gt_180
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def get_range_data(result_dir):
+    result_path = os.path.join(processed_dir, result_dir)
+
+
+    result_files = glob.glob(result_path + '*.tsv')
+    result_data = load_results(result_path, ext='tsv')
+
+    sim_id_pattern="vp.*.json"
+    sim_info_dict = collect_sims_and_results(result_path, sim_id_pattern=sim_id_pattern)
+
+    paf_arr = []
+    tir_arr = []
+    tbr_arr = []
+    tar_arr = []
+
+    for sim in sim_info_dict:
+        sim_data = sim_info_dict[sim]
+        
+        paf_arr.append(sim_data['controller']['config']['partial_application_factor'])
+        
+        result_path = sim_data['result_path']
+        result_data = load_result(result_path)
+
+        tir_arr.append(percent_values_ge_70_le_180(result_data[1]['bg']))
+        tbr_arr.append(percent_values_lt_70(result_data[1]['bg']))
+        tar_arr.append(percent_values_gt_180(result_data[1]['bg']))
+
+
+    paf_arr = np.array(paf_arr)
+
+    x_tir = []
+    x_tbr = []
+    x_tar = []
+    for paf in np.unique(paf_arr):
+        index_list = np.where(paf_arr == paf)[0]
+
+        t = [tir_arr[i] for i in index_list]
+        x_tir.append(t)
+
+        t = [tbr_arr[i] for i in index_list]
+        x_tbr.append(t)
+
+        t = [tar_arr[i] for i in index_list]
+        x_tar.append(t),
+
+    return x_tir, x_tbr, x_tar, paf_arr
+
 processed_dir = os.path.join(DATA_DIR, "processed/")
-result_dir = os.path.join(processed_dir, "autobolus-analysis-results-2024-07-31")
 
-result_files = glob.glob(result_dir + '/patient_id=*.tsv')
+result_dir = 'autobolus-analysis-results-2024-07-31'
+result_dir_with_ma = 'no_meal_announcements_2025_02_07_WITH_MA_140/'
+# result_dir_with_ma = 'no_meal_announcements_2025_02_06_WITH_MA/'
+result_dir_no_ma = 'no_meal_announcements_2025_02_07_NO_MA_140/'
+# result_dir_no_ma = 'no_meal_announcements_2025_02_06_NO_MA/'
 
-result_data = load_results(result_dir, ext='tsv')
+# result_dir_with_ma = 'no_meal_announcements_2025_02_07_NO_MA_140_RC_SHORT/'
 
-sim_info_dict = collect_sims_and_results(result_dir, sim_id_pattern="patient_id.*.json")
-paf_arr = []
-tir_arr = []
-tbr_arr = []
-tar_arr = []
-
-for sim in sim_info_dict:
-    sim_data = sim_info_dict[sim]
-    
-    paf_arr.append(sim_data['controller']['config']['partial_application_factor'])
-    
-    result_path = sim_data['result_path']
-    result_data = load_result(result_path)
-
-    tir_arr.append(percent_values_ge_70_le_180(result_data[1]['bg']))
-    tbr_arr.append(percent_values_lt_70(result_data[1]['bg']))
-    tar_arr.append(percent_values_gt_180(result_data[1]['bg']))
+x_tir_with_ma, x_tbr_with_ma, x_tar_with_ma, paf_arr_with_ma = get_range_data(result_dir_with_ma)
+x_tir_no_ma, x_tbr_no_ma, x_tar_no_ma, paf_arr_no_ma = get_range_data(result_dir_no_ma)
 
 
-paf_arr = np.array(paf_arr)
+print(np.mean(x_tir_with_ma, axis=1)[4])
+print(np.mean(x_tir_no_ma, axis=1)[4])
+
+print(np.std(x_tir_with_ma, axis=1)[4])
+print(np.std(x_tir_no_ma, axis=1)[4])
+
+positions = np.array(range(11))
+widths = 0.3
+colors = ["#FF9999", "#66B3FF"]  # One for each subgroup
+
 fig, ax = plt.subplots(2,1)
 
-x_tir = []
-x_tbr = []
-x_tar = []
-for paf in np.unique(paf_arr):
-    index_list = np.where(paf_arr == paf)[0]
+box1 = ax[0].boxplot(x_tir_with_ma, positions=positions-0.15, widths=widths, patch_artist=True)
+box2 = ax[0].boxplot(x_tir_no_ma, positions=positions+0.15, widths=widths, patch_artist=True)
 
-    t = [tir_arr[i] for i in index_list]
-    x_tir.append(t)
+ax[0].plot(positions-0.15, np.mean(x_tir_with_ma, axis=1), linestyle='None', marker='x', markeredgecolor='Black')
+ax[0].plot(positions+0.15, np.mean(x_tir_no_ma, axis=1), linestyle='None', marker='x', markeredgecolor='Black')
 
-    t = [tbr_arr[i] for i in index_list]
-    x_tbr.append(t)
+for patch1, patch2 in zip(box1["boxes"], box2["boxes"]):
+    patch1.set_facecolor(colors[0])
+    patch2.set_facecolor(colors[1])
 
-    t = [tar_arr[i] for i in index_list]
-    x_tar.append(t)
+# ax[0].set_xticks(positions)
+# ax[0].set_xticklabels(np.unique(paf_arr_with_ma))
 
-ax[0].boxplot(x_tir)
-ax[0].set_xticklabels([])
+ax[0].set_xticks([3.85,4.15])
+ax[0].set_xticklabels(['MA', 'No MA'])
+
 ax[0].set_ylabel('Percent Time in Range\n(70-180 mg/dL)')
 
-ax[1].boxplot(x_tar)
-ax[1].set_xticklabels(np.unique(paf_arr))
+box1 = ax[1].boxplot(x_tar_with_ma, positions=positions-0.15, widths=widths, patch_artist=True)
+box2 = ax[1].boxplot(x_tar_no_ma, positions=positions+0.15, widths=widths, patch_artist=True)
+
+for patch1, patch2 in zip(box1["boxes"], box2["boxes"]):
+    patch1.set_facecolor(colors[0])
+    patch2.set_facecolor(colors[1])
+
+ax[1].set_xticks(positions)
+ax[1].set_xticklabels(np.unique(paf_arr_with_ma))
 ax[1].set_ylabel('Percent Time Above Range\n(>180 mg/dL)')
 ax[1].set_xlabel('Partial Application Factor')
 
---
+++ tidepool_data_science_simulator/projects/autobolus_application_factor/sweep_autobolus_paf.py
@@ -0,0 +1,152 @@
+__author__ = "Mark Connolly"
+
+import logging
+
+import numexpr
+logger = logging.getLogger(__name__)
+
+import time
+import os
+import datetime
+import copy
+from collections import defaultdict
+
+import numpy as np
+from numpy.random import RandomState
+
+
+from tidepool_data_science_simulator.models.simulation import Simulation
+from tidepool_data_science_simulator.models.sensor_icgm import (
+    CLEAN_INITIAL_CONTROLS, iCGM_THRESHOLDS, 
+)
+
+from tidepool_data_science_simulator.models.events import BolusTimeline, CarbTimeline
+from tidepool_data_science_simulator.models.measures import Bolus, Carb, TargetRange
+
+from tidepool_data_science_simulator.visualization.sim_viz import plot_sim_results
+
+from tidepool_data_science_simulator.makedata.make_icgm_patients import transform_icgm_json_to_v2_parser
+from tidepool_data_science_simulator.makedata.scenario_json_parser_v2 import ScenarioParserV2
+
+from tidepool_data_science_simulator.run import run_simulations
+from tidepool_data_science_simulator.utils import DATA_DIR
+
+
+def generate_autobolus_simulations(json_sim_base_config, base_sim_seed):
+    """
+    Generator simulations from a base configuration that have different true bg
+    starting points and different t0 sensor error values.
+    """
+    num_history_values = len(json_sim_base_config["patient"]["sensor"]["glucose_history"]["value"])
+
+    partial_application_factors = [0.3, 0.4, 0.5, 0.6, 0.7]
+    # partial_application_factors = [1]
+
+    true_start_glucose_list = range(50, 250, 10)
+
+    random_state = RandomState(base_sim_seed)
+
+    for paf in partial_application_factors:
+        for initial_blood_glucose in true_start_glucose_list:
+            new_sim_base_config = copy.deepcopy(json_sim_base_config)    
+
+            date_str_format = "%m/%d/%Y %H:%M:%S"  # ref: "8/15/2019 12:00:00"
+            t0 = datetime.datetime.strptime(new_sim_base_config["time_to_calculate_at"], date_str_format)
+
+            glucose_history_values = {i: initial_blood_glucose for i in range(num_history_values)}
+
+            new_sim_base_config["patient"]["sensor"]["glucose_history"]["value"] = glucose_history_values
+            new_sim_base_config["patient"]["patient_model"]["glucose_history"]["value"] = glucose_history_values        
+
+            new_sim_base_config["controller"]["id"] = 'swift'
+            new_sim_base_config["controller"]["settings"]["include_positive_velocity_and_RC"] = True
+            new_sim_base_config["controller"]["settings"]["use_mid_absorption_isf"] = True
+
+            true_carb_timeline = CarbTimeline(datetimes=[t0], events=[Carb(25.0, "U", 240)])
+            np.random.uniform()
+            reported_carb_timeline = CarbTimeline(datetimes=[t0], events=[Carb(25.0, "U", 240)])            
+            
+            sim_parser = ScenarioParserV2()
+            sim_start_time, duration_hrs, virtual_patient, controller = sim_parser.build_components_from_config(new_sim_base_config)
+            
+            virtual_patient.carb_event_timeline = true_carb_timeline
+            virtual_patient.pump.carb_event_timeline = reported_carb_timeline
+            
+            controller.controller_config.controller_settings['minimum_autobolus'] = 0.1
+            controller.controller_config.controller_settings['maximum_autobolus'] = 100
+            controller.controller_config.controller_settings['partial_application_factor'] = paf
+
+            patient_id = new_sim_base_config['patient_id']
+            sim_id="vp={}_patient_id={}_withRC_paf={}_ibg={}".format(base_sim_seed, patient_id, paf, initial_blood_glucose)
+            
+            sim = Simulation(sim_start_time,
+                                duration_hrs=duration_hrs,
+                                virtual_patient=virtual_patient,
+                                controller=controller,
+                                multiprocess=True,
+                                sim_id=sim_id
+                                )
+
+            sim.random_state = random_state
+
+            yield sim
+
+
+def build_autobolus_sim_generator(json_base_configs, sim_batch_size=30):
+    """
+    Build simulations for the FDA AI Letter iCGM sensitivity analysis.
+    """
+    for i, json_config in enumerate(json_base_configs, 1):
+        logger.info("VP: {}. {} of {}".format(json_config["patient_id"], i, len(json_base_configs)))
+
+        sim_ctr = 0
+        sims = {}
+
+        for sim in generate_autobolus_simulations(json_config, base_sim_seed=i):
+
+            sims[sim.sim_id] = sim
+            sim_ctr += 1
+
+            if sim_ctr == sim_batch_size:
+                yield sims
+                sims = {}
+                sim_ctr = 0
+
+        if sims:
+            yield sims
+
+
+if __name__ == "__main__":
+
+    today_timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+    result_dir = os.path.join(DATA_DIR, "processed/autobolus-analysis-results-" + today_timestamp)
+    os.environ['NUMEXPR_MAX_THREADS'] = str(14)
+    numexpr.set_num_threads(14)
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+        logger.info("Made directory for results: {}".format(result_dir))
+
+
+    sim_batch_size = 14
+
+    json_base_configs = transform_icgm_json_to_v2_parser()
+    sim_batch_generator = build_autobolus_sim_generator(json_base_configs, sim_batch_size=sim_batch_size)
+
+    start_time = time.time()
+    for i, sim_batch in enumerate(sim_batch_generator):
+
+        batch_start_time = time.time()
+
+        full_results, summary_results_df = run_simulations(
+            sim_batch,
+            save_dir=result_dir,
+            save_results=True,
+            num_procs=sim_batch_size
+        )
+        batch_total_time = (time.time() - batch_start_time) / 60
+        run_total_time = (time.time() - start_time) / 60
+
+        # plot_sim_results(full_results)
+
+        logger.info("Batch {}".format(i))
+        logger.info("Minutes to build sim batch {} of {} sensors. Total minutes {}".format(batch_total_time, len(sim_batch), run_total_time))
