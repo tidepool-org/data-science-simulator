97fa91547eabdc83ae562adbd53140c4fe746e9a
markjudeconnolly <159162013+markjudeconnolly@users.noreply.github.com>
2025-02-14 15:24:05 -0500

    fixed data location and cleaned up

--- tidepool_data_science_simulator/projects/kalman_filter/kalman_filter.py
+++ tidepool_data_science_simulator/projects/kalman_filter/kalman_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from tidepool_data_science_simulator.evaluation.inspect_results import load_result
 
 from filterpy.kalman import MerweScaledSigmaPoints
@@ -19,7 +20,7 @@ def f_cv(x, dt, u):
     '''
     # Default parameters 
     egp = 1.07 # mg/dL/min
-    egp = 0.18*1 # mg/dL/min
+    egp = 0.18*2 # mg/dL/min
     gezi = 0.0035*0  # 1 / min
     vg = 199  # dL; volume of interstitial space for glucose
     tausen = 10  # min
@@ -33,7 +34,7 @@ def f_cv(x, dt, u):
     ndims = len(x)
     if ndims == 7:
         si = 4.63e-4  # mL/muU
-        si = 0.001  # mL/muU
+        si = 0.0005  # mL/muU
         G, Ieff, Isc, Ip, Gisf, G1, G2 = x
     elif ndims == 8:
         G, Ieff, Isc, Ip, Gisf, G1, G2, si = x
@@ -41,7 +42,7 @@ def f_cv(x, dt, u):
     carb_rate, insulin_rate = u
 
     # Insulin dynamics (2 compartments)
-    dIsc = -Isc/tau1 + insulin_rate/(tau1*ci)  # Eq 1; note tau*ci has units of ml
+    dIsc = -Isc/tau1 + insulin_rate/(tau1*ci)  # Eq 1
     dIp = -Ip/tau2 + Isc/tau2  # Eq 2
     dIeff = -p2*Ieff + p2*si*Ip  # Eq 3
 
@@ -70,11 +71,18 @@ def h_cv(x):
     return np.array([x[0]])
 
 def main():
-    result_path = 'tidepool_data_science_simulator/projects/kalman_filter/icgm_analysis_vp_35_65a71e48e64827646838032c8d29d3ac91ac36da94de018be6f668559ff4f9c2_tbg=70_sbg=150.tsv'
+    result_path = '/Users/mconn/Library/CloudStorage/GoogleDrive-mark.connolly@tidepool.org/My Drive/projects/Sensitivity Analysis/processed_data/icgm_analysis_vp_35_65a71e48e64827646838032c8d29d3ac91ac36da94de018be6f668559ff4f9c2_tbg=70_sbg=150.tsv'
     (_, sim_results_df) = load_result(result_path, ext="tsv")
-    
-    sigma_x = 0
     bgs = np.array(sim_results_df['bg_sensor'])
+    
+    # result_path = '/Users/mconn/Library/CloudStorage/GoogleDrive-mark.connolly@tidepool.org/My Drive/projects/Sensitivity Analysis/processed_data/icgm_user_618.csv'
+    # df = pd.read_csv(result_path)
+    # glucose_df = df.iloc[0]
+    # glucose_np = np.array(glucose_df)
+    # bgs = glucose_np[2:].astype(float)
+    
+    # bgs[int(9200/5)] = 250
+    sigma_x = 1
     y = [np.array([bg + randn()*sigma_x, ]) for bg in bgs]
 
     tt = np.array(range(len(y)))
@@ -103,6 +111,7 @@ def main():
     ukf.Q = np.eye(ndims) * 1e-7 # Uncertainty in the model
     
     if ndims == 8:
+        ukf.Q = np.eye(ndims) * 1e-8 # Uncertainty in the model
         ukf.Q[7,7] = 0 #1e-12*0
 
     uxs = []
@@ -156,7 +165,7 @@ def main():
     uxs = np.array(uxs)
     bg_kf = uxs[:,0]
     
-    _, ax = plt.subplots(2,1)
+    _, ax = plt.subplots(3,1)
     
     ax[0].plot(tt*dt, y, linewidth=2, color='black', label='Sensor')
     ax[0].plot(tt*dt, bg_kf, linewidth=2, color='red', label='Kalman Filter')
@@ -174,7 +183,12 @@ def main():
         ax[1].plot(tt*dt, si_kf, linewidth=2, color='black', label='Si')
         ax[1].set_ylim([0, np.max(si_kf[20:])])
         ax[1].set_xlabel('Minutes')
+        ax[1].set_ylabel('Si (mL/muU)')
+        
     
+    lags = np.arange(-(len(bg_ma)-1), len(y))
+    ax[2].plot(lags, np.correlate(bg_ma, np.squeeze(y), mode='full'))
+    # ax[2].plot(np.correlate(bg_kf, np.squeeze(y), mode='full'))
     plt.show()
 
 if __name__ == "__main__":
