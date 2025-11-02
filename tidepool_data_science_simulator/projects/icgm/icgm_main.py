30eb80a7be44b9371a635e3e5348d61c6eca2a8a
markjudeconnolly <159162013+markjudeconnolly@users.noreply.github.com>
2025-11-02 15:17:44 -0500

    Refactor archiving process in iCGM main script: add directory handling and error management for result archiving and cleanup

--- tidepool_data_science_simulator/projects/icgm/icgm_main.py
+++ tidepool_data_science_simulator/projects/icgm/icgm_main.py
@@ -3,6 +3,7 @@ from multiprocessing import freeze_support
 from tidepool_data_science_simulator.projects.icgm.icgm_analysis_evaluation import process_simulation_data
 from tidepool_data_science_simulator.projects.icgm.icgm_analysis_simulation import run_icgm_simulations
 from tidepool_data_science_simulator.utils import DATA_DIR
+import shutil
 
 
 if __name__ == '__main__':
@@ -25,3 +26,13 @@ if __name__ == '__main__':
     # Process each result directory
     for result_dir in result_dirs:
         summary_csv = process_simulation_data(result_dir)
+
+    parent_dir = os.path.dirname(result_dir)
+    dir_name = os.path.basename(result_dir.rstrip(os.sep))
+    archive_base = os.path.join(parent_dir, dir_name)
+
+    try:
+        shutil.make_archive(archive_base, 'zip', root_dir=parent_dir, base_dir=dir_name)
+        shutil.rmtree(result_dir)
+    except Exception as e:
+        print(f"Failed to archive/delete {result_dir}: {e}")
